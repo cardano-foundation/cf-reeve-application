0a5163d8af7bb333201d3704cbde3d8d79f373ec
Sebastian Bode <sebastian.bode@cardanofoundation.org>
2023-06-13 12:18:32 +0200

    chore: made some corrections to test classed and fix package structure

--- tx-submitter/src/main/java/org/cardanofoundation/lob/txsubmitter/TxSubmitterApplication.java
+++ tx-submitter/src/main/java/org/cardanofoundation/lob/txsubmitter/TxSubmitterApplication.java
@@ -1,4 +1,4 @@
-package org.cardanofoundation.lob.sourceapi;
+package org.cardanofoundation.lob.txsubmitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
