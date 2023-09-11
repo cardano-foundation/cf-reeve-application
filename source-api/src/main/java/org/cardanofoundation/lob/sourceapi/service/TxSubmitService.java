ce7049421da92a0611738b8a4631397b46bd4b86
Marco Russo <marco.russo@cardanofoundation.org>
2023-09-11 16:57:48 +0100

    Sent

--- source-api/src/main/java/org/cardanofoundation/lob/sourceapi/service/TxSubmitService.java
+++ source-api/src/main/java/org/cardanofoundation/lob/sourceapi/service/TxSubmitService.java
@@ -20,6 +20,9 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @// TODO: 11/09/2023 Need to move all this service to tx-submitter 
+ */
 @Component
 @Log4j2
 public class TxSubmitService {
@@ -86,7 +89,6 @@ public class TxSubmitService {
         try {
             result = quickTxBuilder.compose(tx)
                     .withSigner(SignerProviders.signerFrom(sender))                    .complete();
-
 
             waitForTransaction(result);
             checkIfUtxoAvailable(result.getValue(), sender.baseAddress());
