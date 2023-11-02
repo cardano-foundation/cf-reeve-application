32de074747dd6a6427733f00f895c1ac99dd1bc6
Marco Russo <marco.russo@cardanofoundation.org>
2023-11-02 15:26:23 +0000

    The test

--- tx-submitter/src/test/java/org/cardanofoundation/lob/txsubmitter/factory/TxBuilderFactoryTest.java
+++ tx-submitter/src/test/java/org/cardanofoundation/lob/txsubmitter/factory/TxBuilderFactoryTest.java
@@ -1,7 +1,12 @@
 package org.cardanofoundation.lob.txsubmitter.factory;
 
+import com.bloxbean.cardano.client.account.Account;
 import com.bloxbean.cardano.client.backend.api.BackendService;
+import com.bloxbean.cardano.client.metadata.Metadata;
+import com.bloxbean.cardano.client.metadata.MetadataBuilder;
+import com.bloxbean.cardano.client.quicktx.AbstractTx;
 import com.bloxbean.cardano.client.quicktx.QuickTxBuilder;
+import org.cardanofoundation.lob.common.model.TxSubmitJob;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -18,5 +23,27 @@ class TxBuilderFactoryTest {
         QuickTxBuilder quickTxBuilder = txBuilderFactory.createTxBuilder(backendService);
         Assertions.assertInstanceOf(QuickTxBuilder.class, quickTxBuilder);
 
+    }
+
+    @Test
+    void createTx() {
+        Account sender = Mockito.mock(Account.class);
+        TxSubmitJob txSubmitJob = Mockito.mock(TxSubmitJob.class);
+        double random = Math.random();
+
+        Mockito.when(sender.baseAddress()).thenReturn("addr_test1qrq699e8nd9e8jw93kkunh9vmuucaq2qth0xym42ws6sqs468gt7ad8l6dnr9yglcqt5f6ss63wgntfjyl2xpxw0wqpq9xr8tx");
+        Metadata metadata = MetadataBuilder.createMetadata();
+        Mockito.when(txSubmitJob.getTransactionMetadata()).thenReturn(metadata.serialize());
+
+        TxBuilderFactory txBuilderFactory = new TxBuilderFactory();
+        Assertions.assertInstanceOf(AbstractTx.class, txBuilderFactory.createTx(sender, txSubmitJob, random));
+    }
+
+    @Test
+    void createRandom() {
+        TxBuilderFactory txBuilderFactory = new TxBuilderFactory();
+        double randomResult = txBuilderFactory.createRandom();
+        Assertions.assertEquals(Math.random(), randomResult, 1);
+        Assertions.assertNotEquals(0.0d, randomResult);
     }
 }
