28df08abc1159d6e5181c6e2403673ddba4d11b7
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2024-04-03 18:26:30 +0200

    feat: blockchain publisher flag to enable / disable publishing to the blockchain.

--- src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/job/TransactionDispatcherJob.java
+++ src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/job/TransactionDispatcherJob.java
@@ -3,13 +3,14 @@ package org.cardanofoundation.lob.app.blockchain_publisher.job;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cardanofoundation.lob.app.blockchain_publisher.service.dispatch.BlockchainTransactionsDispatcher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "lob.blockchain.publisher.enabled", havingValue = "true")
 public class TransactionDispatcherJob {
 
     private final BlockchainTransactionsDispatcher blockchainTransactionsDispatcher;
--- src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/job/TransactionsWatchDogJob.java
+++ src/main/java/org/cardanofoundation/lob/app/blockchain_publisher/job/TransactionsWatchDogJob.java
@@ -1,11 +1,13 @@
 package org.cardanofoundation.lob.app.blockchain_publisher.job;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@ConditionalOnProperty(value = "lob.blockchain.publisher.enabled", havingValue = "true")
 public class TransactionsWatchDogJob {
 
     @Scheduled(fixedDelayString = "PT10M", initialDelayString = "PT2M")
