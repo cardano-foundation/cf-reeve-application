c1feceb55c477c3ba5f16c4d69640f4545e602ff
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2024-04-19 16:27:21 +0200

    fix: problem module should no longer generate stack traces in rest call responses.a

--- src/main/java/org/cardanofoundation/lob/app/support/web/internal/JsonConfig.java
+++ src/main/java/org/cardanofoundation/lob/app/support/web/internal/JsonConfig.java
@@ -1,27 +1,29 @@
 package org.cardanofoundation.lob.app.support.web.internal;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.zalando.problem.jackson.ProblemModule;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static com.fasterxml.jackson.databind.DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 @Configuration
+@Slf4j
 public class JsonConfig {
 
     @Bean
-    @Primary
-    public ObjectMapper jsonMapper() {
-        return new ObjectMapper()
-                .setSerializationInclusion(NON_NULL)
-                .enable(ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
-                .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .registerModules(new JavaTimeModule(), new Jdk8Module());
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder(ProblemModule problem) {
+        log.info("Configuring Jackson2ObjectMapperBuilder");
+
+        return new Jackson2ObjectMapperBuilder()
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .featuresToEnable(ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, FAIL_ON_UNKNOWN_PROPERTIES)
+                .modulesToInstall(new JavaTimeModule(), new Jdk8Module(), problem);
     }
 
 }
