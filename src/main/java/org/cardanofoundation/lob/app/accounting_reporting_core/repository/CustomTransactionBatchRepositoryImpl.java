2a978ee7bbaf7bc32539e3bb9afb0699052ef6d3
Marco Russo <108673166+M4rc0Russ0@users.noreply.github.com>
2024-06-28 16:57:15 +0100

    [LOB-339] Return batch list order by date (#53)

--- src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/CustomTransactionBatchRepositoryImpl.java
+++ src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/repository/CustomTransactionBatchRepositoryImpl.java
@@ -6,15 +6,21 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.LedgerDispatchStatus;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionType;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.ValidationStatus;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionBatchEntity;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity.TransactionEntity;
 import org.cardanofoundation.lob.app.accounting_reporting_core.resource.requests.BatchSearchRequest;
+import org.cardanofoundation.lob.app.accounting_reporting_core.resource.requests.LedgerDispatchStatusView;
 
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.LedgerDispatchStatus.DISPATCHED;
 import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus.OK;
 
 @RequiredArgsConstructor
@@ -28,38 +34,61 @@ public class CustomTransactionBatchRepositoryImpl implements CustomTransactionBa
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<TransactionBatchEntity> criteriaQuery = builder.createQuery(TransactionBatchEntity.class);
         Root<TransactionBatchEntity> rootEntry = criteriaQuery.from(TransactionBatchEntity.class);
-        Predicate pStatus = builder.isTrue(builder.literal(true));
-        Predicate pTransactionType = builder.isTrue(builder.literal(true));
-        Predicate pAccountingPeriodFrom = builder.isTrue(builder.literal(true));
-        Predicate pAccountingPeriodTo = builder.isTrue(builder.literal(true));
-        Predicate pTransactionStatus = builder.isTrue(builder.literal(true));
-        Predicate pOrganisationId = builder.equal(rootEntry.get("filteringParameters").get("organisationId"), body.getOrganisationId());
+        List<Predicate> andPredicates = new ArrayList<>();
+
+        andPredicates.add(builder.equal(rootEntry.get("filteringParameters").get("organisationId"), body.getOrganisationId()));
 
         if (!body.getStatus().isEmpty()) {
-            pStatus = builder.in(rootEntry.get("status")).value(body.getStatus());
+            Join<TransactionBatchEntity, TransactionEntity> transactionEntityJoin = rootEntry.join("transactions", JoinType.INNER);
+
+            List<Predicate> orPredicates = new ArrayList<>();
+
+            if (0 < body.getStatus().stream().filter(s -> s.equals(LedgerDispatchStatusView.APPROVE)).count()) {
+                orPredicates.add(builder.equal(transactionEntityJoin.get("ledgerDispatchStatus"), LedgerDispatchStatus.MARK_DISPATCH));
+            }
+
+            if (0 < body.getStatus().stream().filter(s -> s.equals(LedgerDispatchStatusView.PENDING)).count()) {
+                orPredicates.add(builder.equal(transactionEntityJoin.get("ledgerDispatchStatus"), LedgerDispatchStatus.NOT_DISPATCHED));
+            }
+
+            if (0 < body.getStatus().stream().filter(s -> s.equals(LedgerDispatchStatusView.INVALID)).count()) {
+                orPredicates.add(builder.equal(transactionEntityJoin.get("automatedValidationStatus"), ValidationStatus.FAILED));
+            }
+
+            if (0 < body.getStatus().stream().filter(s -> s.equals(LedgerDispatchStatusView.PUBLISH)).count()) {
+                orPredicates.add(builder.equal(transactionEntityJoin.get("ledgerDispatchStatus"), LedgerDispatchStatus.DISPATCHED));
+            }
+
+            if (0 < body.getStatus().stream().filter(s -> s.equals(LedgerDispatchStatusView.PUBLISHED)).count()) {
+                orPredicates.add(builder.equal(transactionEntityJoin.get("ledgerDispatchStatus"), LedgerDispatchStatus.COMPLETED));
+            }
+
+            andPredicates.add(builder.or(orPredicates.toArray(new Predicate[0])));
         }
 
         if (!body.getTransactionTypes().isEmpty()) {
             Expression<?> bitwiseAnd = builder.function("BITAND", Integer.class, rootEntry.get("filteringParameters").get("transactionTypes"), builder.literal(body.getTransactionTypes().stream().toList()));
-            pTransactionType = builder.notEqual(bitwiseAnd, 0);
+            andPredicates.add(builder.notEqual(bitwiseAnd, 0));
         }
 
         if (null != body.getFrom()) {
-            pAccountingPeriodFrom = builder.greaterThanOrEqualTo(rootEntry.get("filteringParameters").get("from"), body.getFrom());
+            andPredicates.add(builder.greaterThanOrEqualTo(rootEntry.get("filteringParameters").get("from"), body.getFrom()));
         }
 
         if (null != body.getTo()) {
-            pAccountingPeriodTo = builder.lessThanOrEqualTo(rootEntry.get("filteringParameters").get("to"), body.getTo());
+            andPredicates.add(builder.lessThanOrEqualTo(rootEntry.get("filteringParameters").get("to"), body.getTo()));
         }
 
         if (!body.getTxStatus().isEmpty()) {
-            Join<Object, Object> transactions = rootEntry.join("transactions", JoinType.INNER);
-            pTransactionStatus = builder.in(transactions.get("status")).value(body.getTxStatus());
+            Join<TransactionBatchEntity, TransactionEntity> transactionEntityJoin = rootEntry.join("transactions", JoinType.INNER);
+            andPredicates.add(builder.in(transactionEntityJoin.get("status")).value(body.getTxStatus()));
         }
 
         criteriaQuery.select(rootEntry);
-        criteriaQuery.where(pOrganisationId, pStatus, pTransactionType, pAccountingPeriodFrom, pAccountingPeriodTo, pTransactionStatus);
-
+        criteriaQuery.where(andPredicates.toArray(new Predicate[0]));
+        criteriaQuery.orderBy(builder.desc(rootEntry.get("createdAt")));
+        // Without this line the query only returns one row.
+        criteriaQuery.groupBy(rootEntry.get("id"));
 
         TypedQuery<TransactionBatchEntity> theQuery = em.createQuery(criteriaQuery);
 
--- src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/BatchsDetailView.java
+++ src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/BatchsDetailView.java
@@ -3,12 +3,12 @@ package org.cardanofoundation.lob.app.accounting_reporting_core.resource.views;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Setter
 //@AllArgsConstructor
 public class BatchsDetailView {
     private Long total;
-    private Set<BatchView> batchs;
+    private List<BatchView> batchs;
 }
