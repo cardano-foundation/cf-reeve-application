3bb3856ff2a29d4d27718b9e35cee5bbd4fb19fe
matiwinnetou <mateusz.szczap@gmail.com>
2024-09-16 14:13:27 +0200

    refactor: accounting from and accounting to is now local date. (#89)
    Co-authored-by: Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>

--- cf_netsuite_altavia_erp_connector/build.gradle.kts
+++ cf_netsuite_altavia_erp_connector/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("org.springframework.modulith:spring-modulith-api")
     implementation("org.springframework.modulith:spring-modulith-events-api")
 
+    implementation("org.cardanofoundation:cf-lob-platform-accounting_reporting_core:${property("cfLobPlatformVersion")}")
     implementation("org.cardanofoundation:cf-lob-platform-netsuite_altavia_erp_adapter:${property("cfLobPlatformVersion")}")
     implementation("org.cardanofoundation:cf-lob-platform-organisation:${property("cfLobPlatformVersion")}")
 }
---
+++ cf_netsuite_altavia_erp_connector/src/main/java/org/cardanofoundation/lob/app/cf_netsuite_altavia_erp_connector/package-info.java
@@ -0,0 +1,8 @@
+@org.springframework.lang.NonNullApi
+
+@org.springframework.modulith.ApplicationModule(displayName = "CF NetSuite Altavia Connector", allowedDependencies =
+        { "netsuite_altavia_erp_adapter", "netsuite_altavia_erp_adapter::client", "netsuite_altavia_erp_adapter::repository", "netsuite_altavia_erp_adapter::service", "netsuite_altavia_erp_adapter::domain_core",
+                "organisation",
+                "accounting_reporting_core::service_internal",}
+)
+package org.cardanofoundation.lob.app.cf_netsuite_altavia_erp_connector;
