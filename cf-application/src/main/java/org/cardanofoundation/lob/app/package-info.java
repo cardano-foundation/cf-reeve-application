0dd7a377c03f37b1d59503a998ff98e0650fae7b
matiwinnetou <mateusz.szczap@gmail.com>
2024-09-25 09:34:05 +0200

    feat: blockchain reader / indexer is born (#99)
    * feat: blockchain_reader is born.
    
    * feat: blockchain_reader is born.
    
    ---------
    
    Co-authored-by: Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>

---
+++ cf-application/src/main/java/org/cardanofoundation/lob/app/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.lang.NonNullApi
+
+@org.springframework.modulith.ApplicationModule(displayName = "CF App", allowedDependencies =
+        { "support::spring_web", "support::javers" }
+)
+package org.cardanofoundation.lob.app;
