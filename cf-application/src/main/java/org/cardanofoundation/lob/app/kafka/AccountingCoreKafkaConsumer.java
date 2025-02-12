8e7fbf92a8b375769d44978d3c08b0a5baa6ee55
Thomas Kammerlocher <th.kammerlocher@gmail.com>
2025-02-12 13:43:41 +0100

    feat: add Kafka Consumer and kafka dependencies (#149)
    * chore: add Kafka Consumer and kafka dependencies
    
    * chore: added docker-compose and configurability to disable kafka consumers.
    
    * chore: added docker compose
    
    * chore: updated docker compose
    
    * chore: added configurability to enable or disable kafka. This can extended for other broker as well
    
    * chore: added frontend to docker compose
    
    * chore: updated docker base image
    
    * chore(docker-compose): added traefik as an optional profile to manage SNI via 443
    
    * chore: make kafka default
    
    * chore(docker-compose): fixed conflicting tls conf
    
    * chore: updating base docker image
    
    ---------
    
    Co-authored-by: Roberto C. Morano <roberto.morano@cardanofoundation.org>

---
+++ cf-application/src/main/java/org/cardanofoundation/lob/app/kafka/AccountingCoreKafkaConsumer.java
@@ -0,0 +1,73 @@
+package org.cardanofoundation.lob.app.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.extraction.TransactionBatchChunkEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.extraction.TransactionBatchFailedEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.extraction.TransactionBatchStartedEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.ledger.ReportsLedgerUpdatedEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.ledger.TxsLedgerUpdatedEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.reconcilation.ReconcilationChunkEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.reconcilation.ReconcilationFailedEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.reconcilation.ReconcilationFinalisationEvent;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.event.reconcilation.ReconcilationStartedEvent;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@ConditionalOnProperty(value = {"lob.accounting_reporting_core.enabled", "spring.kafka.enabled"}, havingValue = "true")
+public class AccountingCoreKafkaConsumer {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.ledger.TxsLedgerUpdateEvent")
+    public void listen(TxsLedgerUpdatedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.ledger.ReportsLedgerUpdatedEvent")
+    public void listen(ReportsLedgerUpdatedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.extraction.TransactionBatchFailedEvent")
+    public void listen(TransactionBatchFailedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.extraction.TransactionBatchStartedEvent")
+    public void listen(TransactionBatchStartedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.extraction.TransactionBatchChunkEvent")
+    public void listen(TransactionBatchChunkEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.reconcilation.ReconcilationFailedEvent")
+    public void listen(ReconcilationFailedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.reconcilation.ReconcilationStartedEvent")
+    public void listen(ReconcilationStartedEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.reconcilation.ReconcilationChunkEvent")
+    public void listen(ReconcilationChunkEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+    @KafkaListener(topics = "accounting_reporting_core.domain.event.reconcilation.ReconcilationFinalisationEvent")
+    public void listen(ReconcilationFinalisationEvent message) {
+        applicationEventPublisher.publishEvent(message);
+    }
+
+
+}
