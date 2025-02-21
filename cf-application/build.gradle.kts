2775900bccc6b57c770f6a4325f0215e933f0a34
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-02-21 10:42:24 +0100

    fix: removed dotenv dependency and removed the need of using .env file in docker compose

--- cf-application/build.gradle.kts
+++ cf-application/build.gradle.kts
@@ -5,8 +5,6 @@ val isKafkaEnabled: Boolean = System.getenv("KAFKA_ENABLED")?.toBooleanStrictOrN
 dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    implementation("me.paulschwarz:spring-dotenv:4.0.0")
-
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     //implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
