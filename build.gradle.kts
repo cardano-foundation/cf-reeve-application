a44e8971dd36a71c887de0197faaa19aa075cacb
Florian Schumann <florian.schumann@cardanofoundation.org>
2025-07-30 17:26:22 +0200

    chore(gradle): bump to platform version from merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.1.0-SNAPSHOT-PR355-d25110d-GHRUN16369999371"
+    extra["cfLobPlatformVersion"] = "1.1.0-release-1.1.0-0c669fe-GHRUN16626635679"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
