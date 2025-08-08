db07b1859ad2338d7e417b8f6a87c267b078055b
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-08-08 13:39:06 +0200

    feat: adding sorting and filtering feature

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.1.0-release-1.1.0-d6fcd8a-GHRUN16669156803"
+    extra["cfLobPlatformVersion"] = "1.1.0-release-1.2.0-b66bcc1-GHRUN16829392090"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
