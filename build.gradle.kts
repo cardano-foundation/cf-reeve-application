76a82e7a5911ae5835ed09ff2eabe8ca212bcd11
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-08-13 14:34:06 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.1.0-release-1.2.0-cd835e3-GHRUN16931573647"
+    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-8e2d813-GHRUN16935686331"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
