518df815ef53c761617cb55520ede40cced66352
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-10-03 09:34:37 +0200

    fix: Upgrade to 1.1 to fix missing transactiontype bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
     apply(plugin = "info.solidsoft.pitest")
 
     group = "org.cardanofoundation"
-    version = "1.1.0"
+    version = "1.1.1-rc1"
 
     sourceSets {
         named("main") {
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.1.0"
+    extra["cfLobPlatformVersion"] = "1.1.1-rc1"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
