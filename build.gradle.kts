6056905d2b1292cdc7814a371ad407069ab37d3b
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-10-17 08:28:21 +0200

    chore: upgrading base version

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-8177bb7-GHRUN18524509044"
+    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-9f62fba-GHRUN18560291851"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
