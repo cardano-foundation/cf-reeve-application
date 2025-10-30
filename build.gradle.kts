f242a363c14529bece58d4c22a23b094810bf68a
Marco Russo <108673166+M4rc0Russ0@users.noreply.github.com>
2025-10-30 14:40:11 +0000

    fix/LOB-1703_batch_failed_catch_error (#321)
    * fix/LOB-1703_batch_failed_catch_error
    
    * fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-c2da158-GHRUN18776920543"
+    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-48a2d73-GHRUN18941631918"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
