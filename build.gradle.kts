e7150f88ae41e1e51710b69075ebdbf937faab15
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-11-04 15:09:06 +0100

    Update cfLobPlatformVersion in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-f15e1bf-GHRUN19065369881"
+    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-9ed696f-GHRUN19071334641"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
