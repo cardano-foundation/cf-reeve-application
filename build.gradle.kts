94a6c39e7feca5ee0ac767597c9237115f6bff76
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-11-24 14:45:17 +0100

    chore: using pr docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.2.0-release-1.2.0-57537c3-GHRUN19566172385"
+    extra["cfLobPlatformVersion"] = "1.2.0-PR491-808a3fb-GHRUN19636407399"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
