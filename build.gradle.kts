d88aab245a74b4cd82b777f9373503f7e64a5b15
Marco Russo <108673166+M4rc0Russ0@users.noreply.github.com>
2025-11-26 12:32:37 +0000

    feat: [LOB-1723] Include-local-currency-in-transaction-publishing (#336)

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.3.0-release-1.3.0-47912e1-GHRUN19677014570"
+    extra["cfLobPlatformVersion"] = "1.3.0-release-1.3.0-75e9480-GHRUN19703386268"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
