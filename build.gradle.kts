9a5a2d901b3224993624240dd1b7f9df7747501e
Marco Russo <marco.russo@cardanofoundation.org>
2025-12-10 16:34:26 +0000

    chore:  applying latest changes in platform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.3.0-release-1.3.0-c04bab5-GHRUN19824825601"
+    extra["cfLobPlatformVersion"] = "1.3.0-release-1.3.0-e5c4fa1-GHRUN20059035531"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
