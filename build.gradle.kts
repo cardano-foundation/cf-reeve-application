72b0248b12fca1f626d2adbfce5607893860414d
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-01-26 13:33:22 +0100

    chore: updating version in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
     apply(plugin = "info.solidsoft.pitest")
 
     group = "de.cardanofoundation"
-    version = "1.3.0"
+    version = "1.4.0"
 
     sourceSets {
         named("main") {
@@ -38,8 +38,9 @@ subprojects {
                 includeModule("org.cardanofoundation", "signify")
             }
         }
-        
-        val gitlabMavenRegistryUrl = providers.environmentVariable("GITLAB_MAVEN_REGISTRY_URL").orElse(providers.gradleProperty("gitlabMavenRegistryUrl"))
+
+        val gitlabMavenRegistryUrl = providers.environmentVariable("GITLAB_MAVEN_REGISTRY_URL")
+            .orElse(providers.gradleProperty("gitlabMavenRegistryUrl"))
         if (gitlabMavenRegistryUrl.isPresent()) {
             maven {
                 name = "gitlab"
@@ -61,7 +62,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.2.0"
+    extra["cfLobPlatformVersion"] = "1.3.0"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
