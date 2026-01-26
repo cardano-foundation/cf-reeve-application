97abb0c60f0e8784f4ba80b85b113b49c9cebfc0
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-01-26 13:34:50 +0100

    chore: updating platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ subprojects {
     extra["springBootVersion"] = "3.3.3"
     extra["springCloudVersion"] = "2023.0.0"
     extra["jMoleculesVersion"] = "2023.1.0"
-    extra["cfLobPlatformVersion"] = "1.3.0-PR509-28d567b-GHRUN21284182979"
+    extra["cfLobPlatformVersion"] = "1.4.0-PR537-d42d178-GHRUN21356965534"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
