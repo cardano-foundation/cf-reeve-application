01742b8861e1142bf07c5b3279f6497911e85a23
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-02-19 08:45:31 +0100

    chore: reverting build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ subprojects {
     extra["springCloudVersion"] = "2025.0.1"
     extra["jMoleculesVersion"] = "2023.1.0"
     extra["testcontainers.version"] = "1.21.4"
-    extra["cfLobPlatformVersion"] = "1.4.0-PR564-9a22b2c-GHRUN22170458686"
+    extra["cfLobPlatformVersion"] = "1.4.0-PR536-8f12cff-GHRUN22144442763"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
