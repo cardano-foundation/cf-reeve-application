a4ccb4414ececfd3b244485b8fe3a151e1655734
Luis Zarate <luiszarate@luiss-mbp.speedport.ip>
2026-02-25 17:52:20 +0100

    Adding import CSV transactions with invalid conditions automation tests

--- playwright/tests/steps/importTransactionsCSV.steps.ts
+++ playwright/tests/steps/importTransactionsCSV.steps.ts
@@ -1,76 +1,82 @@
 import {APIResponse, expect} from '@playwright/test';
-import {createBdd} from 'playwright-bdd'
+import {Given, When, Then} from "../../utils/playwright-bdd-fixtures";
 import {reeveService} from "../../api/reeve-api/reeve.service";
 import {HttpStatusCodes} from "../../api/api-helpers/http-status-codes";
 import {transactionsBuilder} from "../../helpers/transactionsBuilder";
 import {BatchesStatusCodes} from "../../api/api-helpers/batches-status-codes";
-import {TransactionItemCsvDto} from "../../api/dtos/transactionItemCsvDto";
 import {BatchResponse} from "../../api/dtos/batchDto";
 import {transactionValidator} from "../../validators/transactionValidator";
-import {TransactionPendingStatus} from "../../helpers/transaction-pending-status";
+import {TransactionPendingInvalidStatus} from "../../helpers/transaction-pending-invalid-status";
 import {deleteFile} from "../../utils/csvFileGenerator";
 
-const {Given, When, Then} = createBdd();
-let authToken: string
-let transactionCSVFile: string;
-let transactionDataToImport: TransactionItemCsvDto[] = [];
-Given(/^Manager user wants to import a transaction with a CSV file$/, async ({request}) => {
+Given(/^Manager user wants to import a transaction with a CSV file$/, async ({request, ctx}) => {
     const loginResponse = await (await reeveService(request)).loginManager()
     expect(loginResponse.status()).toEqual(HttpStatusCodes.success)
-    authToken = (await loginResponse.json()).token_type + " " + (await loginResponse.json()).access_token;
+    ctx.authToken = (await loginResponse.json()).token_type + " " + (await loginResponse.json()).access_token;
 });
-Given(/^the manager creates the CSV file with all the required fields$/, async ({request}) => {
-    transactionCSVFile = await (await transactionsBuilder(request, authToken))
-        .createReadyToApproveTransaction(transactionDataToImport);
+Given(/^the manager creates the CSV file with all the required fields$/, async ({request,ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createReadyToApproveTransaction(ctx.transactionDataToImport);
 });
-Given(/^system get the validation request$/, async ({request}) => {
-    const validateResponse = await (await reeveService(request)).validateTransactionCsvFile(authToken,
-        transactionCSVFile);
+Given(/^system get the validation request$/, async ({request,ctx}) => {
+    const validateResponse = await (await reeveService(request)).validateTransactionCsvFile(ctx.authToken,
+        ctx.transactionCSVFile);
     expect(validateResponse.status()).toEqual(HttpStatusCodes.success);
 });
-When(/^system get import request$/, async ({request}) => {
-    const importTxCsvResponse = await (await reeveService(request)).importTransactionCsvFile(authToken,
-        transactionCSVFile);
+When(/^system get import request$/, async ({request, ctx}) => {
+    const importTxCsvResponse = await (await reeveService(request)).importTransactionCsvFile(ctx.authToken,
+        ctx.transactionCSVFile);
     expect(importTxCsvResponse.status()).toEqual(HttpStatusCodes.RequestAccepted);
-    await deleteFile(transactionCSVFile)
+    await deleteFile(ctx.transactionCSVFile)
 });
-Then(/^the transaction data should be imported with ready to approve status$/, async ({request}) => {
-    const newBatchAfterImport = await (await reeveService(request)).getNewBatch(authToken,
-        BatchesStatusCodes.APPROVE, transactionDataToImport[0].TxNumber);
-    const batchDetailsResponse = await (await reeveService(request)).getBatchById(authToken,
+Then(/^the transaction data should be imported with ready to approve status$/, async ({request, ctx}) => {
+    const newBatchAfterImport = await (await reeveService(request)).getNewBatch(ctx.authToken,
+        BatchesStatusCodes.APPROVE, ctx.transactionDataToImport[0].TxNumber);
+    const batchDetailsResponse = await (await reeveService(request)).getBatchById(ctx.authToken,
         newBatchAfterImport.id);
     expect(batchDetailsResponse.status()).toEqual(HttpStatusCodes.success);
     let importedBatchDetails: BatchResponse =  await batchDetailsResponse.json()
-    await (await transactionValidator()).validateImportedTxWithStatus(transactionDataToImport, importedBatchDetails,
+    await (await transactionValidator()).validateImportedTxWithStatus(ctx.transactionDataToImport, importedBatchDetails,
         BatchesStatusCodes.APPROVE);
 });
-Given(/^the cost center data in the CSV file doesn't exist in the system$/, async ({request}) => {
-    transactionCSVFile = await (await transactionsBuilder(request, authToken))
-        .createCSVTransactionPending(transactionDataToImport, TransactionPendingStatus.COST_CENTER_DATA_NOT_FOUND);
+Given(/^the cost center data in the CSV file doesn't exist in the system$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionPending(ctx.transactionDataToImport, TransactionPendingInvalidStatus.COST_CENTER_DATA_NOT_FOUND);
 });
-Then(/^the system should create the transaction with pending status by "([^"]*)"$/, async ({request}, reason) => {
-    const newBatchAfterImport = await (await reeveService(request)).getNewBatch(authToken,
-        BatchesStatusCodes.PENDING, transactionDataToImport[0].TxNumber);
-    await (await transactionValidator()).validateImportedTxWithStatus(transactionDataToImport, newBatchAfterImport,
+Then(/^the system should create the transaction with pending status by "([^"]*)"$/, async ({request, ctx}, reason) => {
+    const newBatchAfterImport = await (await reeveService(request)).getNewBatch(ctx.authToken,
+        BatchesStatusCodes.PENDING, ctx.transactionDataToImport[0].TxNumber);
+    await (await transactionValidator()).validateImportedTxWithStatus(ctx.transactionDataToImport, newBatchAfterImport,
         BatchesStatusCodes.PENDING);
-    if(reason == TransactionPendingStatus.COST_CENTER_DATA_NOT_FOUND){
-        await (await transactionValidator()).validatePendingCondition(newBatchAfterImport,
-            TransactionPendingStatus.COST_CENTER_DATA_NOT_FOUND)
-    }
-    if(reason == TransactionPendingStatus.VAT_DATA_NOT_FOUND){
-        await (await transactionValidator()).validatePendingCondition(newBatchAfterImport,
-            TransactionPendingStatus.VAT_DATA_NOT_FOUND)
-    }
-    if(reason == TransactionPendingStatus.CHART_OF_ACCOUNT_NOT_FOUND){
-        await (await transactionValidator()).validatePendingCondition(newBatchAfterImport,
-            TransactionPendingStatus.CHART_OF_ACCOUNT_NOT_FOUND)
-    }
+    await (await transactionValidator()).validatePendingCondition(newBatchAfterImport,
+        reason)
 });
-Given(/^the vat code data in the CSV file doesn't exist in the system$/, async ({request}) => {
-    transactionCSVFile = await (await transactionsBuilder(request, authToken))
-        .createCSVTransactionPending(transactionDataToImport, TransactionPendingStatus.VAT_DATA_NOT_FOUND);
+Given(/^the vat code data in the CSV file doesn't exist in the system$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionPending(ctx.transactionDataToImport, TransactionPendingInvalidStatus.VAT_DATA_NOT_FOUND);
 });
-Given(/^the chart of account code data in the CSV file doesn't exist in the system$/, async ({request}) => {
-    transactionCSVFile = await (await transactionsBuilder(request, authToken))
-        .createCSVTransactionPending(transactionDataToImport, TransactionPendingStatus.CHART_OF_ACCOUNT_NOT_FOUND);
+Given(/^the chart of account code data in the CSV file doesn't exist in the system$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionPending(ctx.transactionDataToImport, TransactionPendingInvalidStatus.CHART_OF_ACCOUNT_NOT_FOUND);
+});
+Given(/^the transaction amounts are not balanced$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionInvalid(ctx.transactionDataToImport, TransactionPendingInvalidStatus.UNBALANCED_TRANSACTION);
+});
+Then(/^the system should create the transaction with invalid status by "([^"]*)"$/, async ({request, ctx}, reason) => {
+    const newBatchAfterImport = await (await reeveService(request)).getNewBatchByDocumentNumber(ctx.authToken,
+        BatchesStatusCodes.INVALID, ctx.transactionDataToImport[0].DocumentName);
+    await (await transactionValidator()).validateImportedTxWithStatus(ctx.transactionDataToImport, newBatchAfterImport,
+        BatchesStatusCodes.INVALID);
+    await (await transactionValidator()).validateInvalidCondition(newBatchAfterImport,
+        reason)
+});
+Given(/^the transaction number is missing in the CSV file$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionInvalid(ctx.transactionDataToImport, TransactionPendingInvalidStatus.TX_INTERNAL_NUMBER_MUST_BE_PRESENT);
+
+});
+Given(/^the transaction debit code is missing in the CSV file$/, async ({request, ctx}) => {
+    ctx.transactionCSVFile = await (await transactionsBuilder(request, ctx.authToken))
+        .createCSVTransactionInvalid(ctx.transactionDataToImport, TransactionPendingInvalidStatus.ACCOUNT_CODE_DEBIT_IS_EMPTY);
 });
