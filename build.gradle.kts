eb5d26e76f8044615e9afb395fa1f33946c1b8e4
Marco Russo <marco.russo@cardanofoundation.org>
2026-03-13 13:27:10 +0000

    fix [LOB-2034] Cumulative Summing error in IS (prod)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
     apply(plugin = "info.solidsoft.pitest")
 
     group = "de.cardanofoundation"
-    version = "1.4.0"
+    version = "1.4.2"
 
     sourceSets {
         named("main") {
@@ -73,7 +73,7 @@ subprojects {
     extra["springCloudVersion"] = "2025.0.1"
     extra["jMoleculesVersion"] = "2023.1.0"
     extra["testcontainers.version"] = "1.21.4"
-    extra["cfLobPlatformVersion"] = "1.4.1-PR609-cdb755f-GHRUN23050268186"
+    extra["cfLobPlatformVersion"] = "1.4.2"
     extra["flyway.version"] = "10.20.1"
 
     dependencies {
