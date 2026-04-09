80378f84d13131563dfa642c4837160fad03861b
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2026-04-09 09:15:26 +0200

    chore: added env vars to docker-compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ subprojects {
     extra["springCloudVersion"] = "2025.0.1"
     extra["jMoleculesVersion"] = "2023.1.0"
     extra["testcontainers.version"] = "1.21.4"
-    extra["cfLobPlatformVersion"] = "1.5.0-PR621-15e5c89-GHRUN24129724732"
+    extra["cfLobPlatformVersion"] = "1.5.0-PR615-4650c3d-GHRUN24129847225"
 
     dependencies {
         compileOnly("org.projectlombok:lombok:1.18.32")
