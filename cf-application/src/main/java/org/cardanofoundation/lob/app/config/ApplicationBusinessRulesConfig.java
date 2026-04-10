802cb511a4ef61987cd3048530a5c717b283b951
Marco Russo <marco.russo@cardanofoundation.org>
2026-04-10 10:28:48 +0100

    chore: making this work

--- cf-application/src/main/java/org/cardanofoundation/lob/app/config/ApplicationBusinessRulesConfig.java
+++ cf-application/src/main/java/org/cardanofoundation/lob/app/config/ApplicationBusinessRulesConfig.java
@@ -7,6 +7,7 @@ import org.cardanofoundation.lob.app.accounting_reporting_core.service.business_
 import org.cardanofoundation.lob.app.accounting_reporting_core.service.business_rules.PipelineTask;
 import org.cardanofoundation.lob.app.accounting_reporting_core.service.business_rules.items.*;
 import org.cardanofoundation.lob.app.organisation.OrganisationPublicApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -14,11 +15,18 @@ import java.util.List;
 @Configuration
 public class ApplicationBusinessRulesConfig extends BusinessRulesConfig {
 
+    @Value("${lob.accounting_reporting_core.rules.cardano_rules:false}")
+    private boolean cardanoRules;
+
     public ApplicationBusinessRulesConfig(Validator validator, OrganisationPublicApi organisationPublicApi, CoreCurrencyRepository currencyRepository) {
         super(validator, organisationPublicApi, currencyRepository);
     }
 
+    @Override
     protected PipelineTask preValidationPipelineTask() {
+        if (cardanoRules) {
+            return super.preValidationPipelineTask();
+        }
         return new DefaultPipelineTask(List.of(
                 new AmountsFcyCheckTaskItem(),
                 new AmountsLcyCheckTaskItem(),
