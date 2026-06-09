d8b2a61be1155782c6fa229b4d9b22731db139df
Marco Russo <marco.russo@cardanofoundation.org>
2026-06-09 12:00:38 +0100

    fix config

--- cf-application/build.gradle.kts
+++ cf-application/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.cardanofoundation:cf-lob-platform-blockchain_publisher:${property("cfLobPlatformVersion")}")
     implementation("org.cardanofoundation:cf-lob-platform-accounting_reporting_core:${property("cfLobPlatformVersion")}")
     implementation("org.cardanofoundation:cf-lob-platform-reporting:${property("cfLobPlatformVersion")}")
+    implementation("org.cardanofoundation:cf-lob-platform-funding:${property("cfLobPlatformVersion")}")
 }
 
 
